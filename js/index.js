11ea4299bab752ae2d662ad7a50c75b93d271c38
unknown <samantha.c.egge@gmail.com>
2019-05-14 13:56:01 -0700

    Completed no click on Nav Tags. Added dblclick, mouseover, wheel event listeners. Added photo to img folder.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
 // Your code goes here
+  
+// Destination Button change to red font
+const signUpButton = document.querySelectorAll('.destination > .btn')
+
+signUpButton.forEach(each => {
+    each.addEventListener('mouseover', function (event, index) {
+    each.style.color = 'red';})
+})
+
+// Doubleclick on page, receive dog, ????, profit
+const bodyWork = document.querySelector('body')
+
+bodyWork.addEventListener('dblclick', function () {
+    bodyWork.style.backgroundImage = "url('../img/DSC_0645.jpg')"
+    ;
+    bodyWork.style.backgroundImage.width = '10%';
+    console.log('hi')
+})
+
+// Rotate Bus Image by Scroll
+const busImage = document.querySelector('.intro > img')
+
+busImage.addEventListener ('wheel', function () {
+    busImage.style.transform = 'rotate(180deg)';
+})
+
+// Remove ability to click on Nav Elements
+const navItems = document.querySelector('nav')
+
+navItems.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('hi')
+    return false;
+  })
+
+navItems.addEventListener('auxclick', function (event) {
+    event.preventDefault();
+    console.log('hi')
+    return false;
+})
+
+navItems.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    console.log('hi')
+    return false;
+})
+
+
